6e651230dc1229a34d30823fafd869f2bf5ce182
Olha Malynovska <olha.malynovska@eleks.com>
2022-12-23 14:59:21 +0200

    my first repo - commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#eleks = {"Pavlo", "Bogdan", "Anton", "Dmytro", "Anna", "Alla", "Artem"}
+#toshiba ={"Kasja", "Dasja", "Anton", "Alla", "Vasja", "Asja", "Artem"}
+
+#Toshiba.update(Eleks)
+#print(Toshiba)
+
+
+# casino_blacklist =["alla black", "anna white", "alla blue", "anna red", "alla red","anna sydor","anna pekar"]
+# poker_blacklist =[ "anna white", "alla black","alla blue","anna pekar", "aja red", "alla red"]
+# alcohol_blacklist= [ "anna white", "alla blue", "anna red", "anna black","inna red", "anna pekar"]
+#
+#
+# i = 0
+# while  i <= len(casino_blacklist)-1:
+#     if casino_blacklist [i] in poker_blacklist and casino_blacklist [i] in alcohol_blacklist :
+#          print (casino_blacklist [i])
+#     i += 1
+
+# friends = ["John", "Marta", "James"]
+# enemies = ["John", "Johnatan", "Artur"]
+
+
+#for i in range(len(friends)):
+ #   print(f'{friends[i]},','we are the best friends :)')
+
+#for x in range(len(enemies)):
+#    print(f'{enemies[x]},','we are the best friends :(')
+
+# original_String = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
+# print("The original string is ",original_String)
+# items = original_String.split('&')
+# dict = {}
+# for i in items:
+#     a = i.split('=')
+#     if len(a) > 1:
+#         key = a[0].strip()
+#         val = a[1].strip()
+#         dict[key] = val
+# print("The result", dict)
+
+
+#txt="john peter brian Morgan Adam Maria bart"
+#x = txt.title()
+#print(x)
